18af2c12a69831efecf1c9d74ca990397d5630f2
smart-cau <1995hyunwoo@naver.com>
2024-08-05 12:49:39 +0000

    test: test 용이하게 순서 변경 및 삭제 검증 코드에 rb_constraint 추가

--- test/test-rbtree.c
+++ test/test-rbtree.c
@@ -311,17 +311,19 @@ void test_to_array_suite() {
 }
 
 void test_find_erase(rbtree *t, const key_t *arr, const size_t n) {
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++) {    
     node_t *p = rbtree_insert(t, arr[i]);
     assert(p != NULL);
   }
-
   for (int i = 0; i < n; i++) {
     node_t *p = rbtree_find(t, arr[i]);
+
     // printf("arr[%d] = %d\n", i, arr[i]);
     assert(p != NULL);
     assert(p->key == arr[i]);
     rbtree_erase(t, p);
+    test_color_constraint(t);
+    test_search_constraint(t);
   }
 
   for (int i = 0; i < n; i++) {
@@ -371,13 +373,13 @@ int main(void) {
   test_init();
   test_insert_single(1024);
   test_find_single(512, 1024);
-  test_erase_root(128);
-  test_find_erase_fixed();
-  test_minmax_suite();
-  test_to_array_suite();
+  test_erase_root(128);  
+  test_minmax_suite();  
   test_distinct_values();
   test_duplicate_values();
-  test_multi_instance();
+  test_find_erase_fixed();
+  // test_to_array_suite();
+  // test_multi_instance();
   test_find_erase_rand(10000, 17);
   printf("Passed all tests!\n");
 }
